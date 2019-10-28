e1eb68d56e62295b9c0467685b1ce0c82a8195ce
insignare <leclay.yo@gmail.com>
2019-10-28 19:45:33 +0100

    add - interface Writer

---
+++ src/com/insi/homeshop/Writer.java
@@ -0,0 +1,19 @@
+package com.insi.homeshop;
+
+public interface Writer {
+    /**
+     * Start writing process
+     */
+    public void start();
+
+    /**
+     * write on line
+     * @param line
+     */
+    public void writeLine(String line);
+
+    /**
+     * Stop writing process
+     */
+    public void stop();
+}
